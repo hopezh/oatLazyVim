e3af0c4c27a92615668eff6f4d4040528c87c237
hopezh <hope.zh@gmail.com>
2023-05-27 21:43:17 +0800

    simplify method to change indent blankline options

--- lua/plugins/indent_blankline.lua
+++ lua/plugins/indent_blankline.lua
@@ -2,16 +2,28 @@ return {
     { -- indent-blankline -------------------------------------------------------
         "lukas-reineke/indent-blankline.nvim",
 
-        config = function()
-            require("indent_blankline").setup({
-                char = "▏", -- indent symbol
-                -- char = "│",
+        -- method 1 to change some options
+        opts = {
+            char = "▏", -- indent symbol
+            -- char = "│",
 
-                show_current_context = true, -- highlight the context
-                show_current_context_start = false,
+            show_current_context = true, -- highlight the context
+            show_current_context_start = false,
 
-                show_trailing_blankline_indent = false,
-            })
-        end,
+            show_trailing_blankline_indent = false,
+        },
+
+        -- method 2
+        -- config = function()
+        --     require("indent_blankline").setup({
+        --         char = "▏", -- indent symbol
+        --         -- char = "│",
+        --
+        --         show_current_context = true, -- highlight the context
+        --         show_current_context_start = false,
+        --
+        --         show_trailing_blankline_indent = false,
+        --     })
+        -- end,
     },
 }
