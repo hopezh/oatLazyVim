ed70f69530255a9d32d7a8311fbf08e81aee00d7
hopezh <hope.zh@gmail.com>
2023-05-27 21:58:08 +0800

    use opts to change options for nvim-notify

--- lua/plugins/nvim_notify.lua
+++ lua/plugins/nvim_notify.lua
@@ -2,9 +2,14 @@ return {
     {
         "rcarriga/nvim-notify",
 
-        require("notify").setup({
+        opts = {
             -- when using transparent in tokyonight, need to set a bg color here
             background_colour = "#000000",
-        }),
+        },
+
+        -- require("notify").setup({
+        --     -- when using transparent in tokyonight, need to set a bg color here
+        --     background_colour = "#000000",
+        -- }),
     },
 }
