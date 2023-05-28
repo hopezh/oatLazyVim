c6bad773f9510fb6b8af6b8220c6ec1e9d12da44
hopezh <hope.zh@gmail.com>
2023-05-28 22:36:41 +0800

    no transparency for cursorline number

--- lua/plugins/transparent.lua
+++ lua/plugins/transparent.lua
@@ -12,15 +12,34 @@ return {
     },
 
     opts = {
+        groups = { -- table: default groups
+            "Normal",
+            "NormalNC",
+            "Comment",
+            "Constant",
+            "Special",
+            "Identifier",
+            "Statement",
+            "PreProc",
+            "Type",
+            "Underlined",
+            "Todo",
+            "String",
+            "Function",
+            "Conditional",
+            "Repeat",
+            "Operator",
+            "Structure",
+            "LineNr",
+            "NonText",
+            "SignColumn",
+            "EndOfBuffer",
+            -- 'CursorLineNr',
+        },
+
         extra_groups = {
             "NormalFloat", -- plugins which have float panel such as Lazy, Mason, LspInfo
             "NvimTreeNormal", -- NvimTree
         },
     },
-
-    -- config = function()
-    --     require("transparent").setup({
-    --         --
-    --     })
-    -- end,
 }
