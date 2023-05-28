0294b7624d2a0772bb944bd317761c45e12a6d07
hopezh <hope.zh@gmail.com>
2023-05-28 22:36:04 +0800

    remove vim-tmux-navigator

--- lua/plugins/vim_tmux_navigator.lua
+++
@@ -1,9 +0,0 @@
-return {
-  {
-    "christoomey/vim-tmux-navigator",
-    config = function()
-      -- the following is not working
-      -- require("vim-tmux-navigator").setup({})
-    end,
-  },
-}
