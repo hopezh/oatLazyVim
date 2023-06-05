b0c277b832b1103f2ecbc080b2c4b0b0433914ce
hopezh <hope.zh@gmail.com>
2023-06-05 11:25:15 +0800

    show hidden files, e.g. dotfiles, in telescope search

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,10 @@
+return {
+    "nvim-telescope/telescope.nvim",
+
+    opts = {
+        pickers = {
+            -- show hidden files in search
+            find_files = { hidden = true },
+        },
+    },
+}
