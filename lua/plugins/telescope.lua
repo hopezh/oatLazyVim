39c9f3247a4d77feed5181d6ebdc97faf7e90fab
hopezh <hope.zh@gmail.com>
2023-06-29 17:07:55 +0800

    use horizontal layout with prompt on top

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,9 +2,19 @@ return {
     "nvim-telescope/telescope.nvim",
 
     opts = {
+        -- change some default options
+        defaults = {
+            layout_strategy = "horizontal",
+            layout_config = {
+                prompt_position = "top",
+                preview_width = 100,
+            },
+            sorting_strategy = "ascending",
+            winblend = 0,
+        },
+
         pickers = {
-            -- show hidden files in search
-            find_files = { hidden = true },
+            find_files = { hidden = true }, -- show hidden files in search
         },
     },
 }
