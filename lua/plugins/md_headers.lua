4cff0f01f56c67884ac82ef6198ea2842667e9ba
Ji ZHANG <hope.zh@gmail.com>
2023-06-30 14:44:29 +0800

    install md-headers.nvim

---
+++ lua/plugins/md_headers.lua
@@ -0,0 +1,26 @@
+return {
+    "AntonVanAssche/md-headers.nvim",
+    lazy = false,
+
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+    },
+
+    keys = {
+        {
+            "<leader>mdh",
+            "<cmd>MarkdownHeaders<CR>",
+            desc = "Show all markdown headers",
+        },
+
+        {
+            "<leader>mdc",
+            "<cmd>MarkdownHeadersClosest<CR>",
+            desc = "Show current md header in context",
+        },
+    },
+
+    config = function()
+        require("md-headers").setup()
+    end,
+}
