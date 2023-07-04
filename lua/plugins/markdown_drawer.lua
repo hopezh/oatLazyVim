8ac131d76349a3eec19a57b1298116bcec7b6002
Ji ZHANG <hope.zh@gmail.com>
2023-07-04 17:01:47 +0800

    install markdown_drawer

---
+++ lua/plugins/markdown_drawer.lua
@@ -0,0 +1,12 @@
+return {
+    "Scuilion/markdown-drawer",
+    lazy = false,
+
+    keys = {
+        {
+            "<leader>mdr",
+            "<cmd>MarkDrawer<CR>",
+            desc = "Draw markdown headers",
+        },
+    },
+}
