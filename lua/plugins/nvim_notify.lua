6fb0fe858a6a6b10e3ff28028e77b9d49f7dc2f4
hopezh <hope.zh@gmail.com>
2023-07-07 21:16:22 +0800

    add a comment in nvim_notify

--- lua/plugins/nvim_notify.lua
+++ lua/plugins/nvim_notify.lua
@@ -7,6 +7,7 @@ return {
             background_colour = "#000000",
         },
 
+        -- the above is equivalent to the following:
         -- require("notify").setup({
         --     -- when using transparent in tokyonight, need to set a bg color here
         --     background_colour = "#000000",
