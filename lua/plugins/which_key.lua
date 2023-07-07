1a0cb49df45c663cab8864372cd6e73ab49187b2
hopezh <hope.zh@gmail.com>
2023-07-07 23:25:58 +0800

    update which-key config to use the default as base

--- lua/plugins/which_key.lua
+++ lua/plugins/which_key.lua
@@ -1,8 +1,47 @@
 return {
     "folke/which-key.nvim",
 
-    opts = function()
-        require("which-key").register({
+    -- opts = function()
+    --     require("which-key").register({
+    --         -- add keymap group for those keybindings not using <leader>
+    --         ["z"] = { name = "+fold" },
+    --
+    --         -- add keymap group and keybindings using <leader>
+    --         ["<leader>z"] = {
+    --             name = "OatKey",
+    --             m = {
+    --                 "<cmd>CellularAutomaton make_it_rain<CR>",
+    --                 "Make it rain...",
+    --             },
+    --             g = {
+    --                 "<cmd>CellularAutomaton game_of_life<CR>",
+    --                 "Game of life...",
+    --             },
+    --         },
+    --     })
+    -- end,
+
+    opts = {
+        plugins = { spelling = true },
+
+        defaults = {
+            mode = { "n", "v" },
+            ["g"] = { name = "+goto" },
+            ["gz"] = { name = "+surround" },
+            ["]"] = { name = "+next" },
+            ["["] = { name = "+prev" },
+            ["<leader><tab>"] = { name = "+tabs" },
+            ["<leader>b"] = { name = "+buffer" },
+            ["<leader>c"] = { name = "+code" },
+            ["<leader>f"] = { name = "+file/find" },
+            ["<leader>g"] = { name = "+git" },
+            ["<leader>gh"] = { name = "+hunks" },
+            ["<leader>q"] = { name = "+quit/session" },
+            ["<leader>s"] = { name = "+search" },
+            ["<leader>u"] = { name = "+ui" },
+            ["<leader>w"] = { name = "+windows" },
+            ["<leader>x"] = { name = "+diagnostics/quickfix" },
+
             -- add keymap group for those keybindings not using <leader>
             ["z"] = { name = "+fold" },
 
@@ -18,6 +57,12 @@ return {
                     "Game of life...",
                 },
             },
-        })
+        },
+    },
+
+    config = function(_, opts)
+        local wk = require("which-key")
+        wk.setup(opts)
+        wk.register(opts.defaults)
     end,
 }
