e2062a13ec608fd0c4dbb4b7b31b0249153b74a2
hopezh <hope.zh@gmail.com>
2023-07-30 20:38:16 +0800

    add comments to marksman config in nvim-lspconfig

--- lua/plugins/nvim_lspconfig.lua
+++ lua/plugins/nvim_lspconfig.lua
@@ -12,6 +12,10 @@ return {
                 r_language_server = {},
                 julials = {},
                 marksman = {
+                    -- also needs:
+                    -- $home/.config/marksman/config.toml :
+                    -- [core]
+                    -- markdown.file_extensions = ["md", "markdown", "qmd"]
                     filetypes = { "markdown", "quarto" },
                     root_dir = require("lspconfig.util").root_pattern(".git", ".marksman.toml", "_quarto.yml"),
                 },
