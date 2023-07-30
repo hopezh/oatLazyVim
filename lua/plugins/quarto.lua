43802322978ec867314fc3f10f3a769ee897118b
hopezh <hope.zh@gmail.com>
2023-07-30 18:42:26 +0800

    add quarto-nvim opts

--- lua/plugins/quarto.lua
+++ lua/plugins/quarto.lua
@@ -5,13 +5,33 @@ return {
         dev = false,
         ft = "quarto",
 
-        -- dependencies = {
-        --     "neovim/nvim-lspconfig",
-        -- },
+        dependencies = {
+            "neovim/nvim-lspconfig",
+            "jmbuhr/otter.nvim",
+            "hrsh7th/nvim-cmp",
+        },
 
         opts = {
+            debug = false,
+            closePreviewOnExit = true,
             lspFeatures = {
+                enabled = true,
                 languages = { "r", "python", "julia", "bash", "lua", "html" },
+                chunks = "curly", -- 'curly' or 'all'
+                diagnostics = {
+                    enabled = true,
+                    triggers = { "BufWritePost" },
+                },
+                completion = {
+                    enabled = true,
+                },
+            },
+            keymap = {
+                hover = "K",
+                definition = "gd",
+                -- rename = '<leader>lR', -- default of quarto-nvim
+                rename = "<leader>cr",
+                references = "gr",
             },
         },
 
@@ -27,18 +47,42 @@ return {
     -- config for dependincies -------------------------------------------------
     {
         "jmbuhr/otter.nvim",
+        lazy = false,
         opts = {},
     },
 
     {
         "hrsh7th/nvim-cmp",
         dependencies = { "jmbuhr/otter.nvim" },
+        ---@param opts cmp.ConfigSchema
         opts = function(_, opts)
-            ---@param opts cmp.ConfigSchema
             local cmp = require("cmp")
             opts.sources = cmp.config.sources(vim.list_extend(opts.sources, { { name = "otter" } }))
         end,
     },
+
+    -- {
+    --     "neovim/nvim-lspconfig",
+    --
+    --     ---@class PluginLspOpts
+    --     opts = {
+    --         ---@type lspconfig.options
+    --         servers = {
+    --             -- these lsp will be automatically installed with mason and loaded with lspconfig
+    --             pyright = {},
+    --             r_language_server = {},
+    --             julials = {},
+    --             marksman = {
+    --                 -- also needs:
+    --                 -- $home/.config/marksman/config.toml :
+    --                 -- [core]
+    --                 -- markdown.file_extensions = ["md", "markdown", "qmd"]
+    --                 filetypes = { "markdown", "quarto" },
+    --                 root_dir = require("lspconfig.util").root_pattern(".git", ".marksman.toml", "_quarto.yml"),
+    --             },
+    --         },
+    --     },
+    -- },
 
     -- send code from python/r/qmd documets to a terminal or REPL
     -- like ipython, R, bash
@@ -52,15 +96,15 @@ return {
             end
 
             vim.cmd([[
-            let g:slime_dispatch_ipython_pause = 100
-            function SlimeOverride_EscapeText_quarto(text)
-            call v:lua.Quarto_is_in_python_chunk()
-            if exists('g:slime_python_ipython') && len(split(a:text,"\n")) > 1 && b:quarto_is_python_chunk
-            return ["%cpaste -q\n", g:slime_dispatch_ipython_pause, a:text, "--", "\n"]
-            else
-            return a:text
-            end
-            endfunction
+                let g:slime_dispatch_ipython_pause = 100
+                function SlimeOverride_EscapeText_quarto(text)
+                    call v:lua.Quarto_is_in_python_chunk()
+                    if exists('g:slime_python_ipython') && len(split(a:text,"\n")) > 1 && b:quarto_is_python_chunk
+                    return ["%cpaste -q\n", g:slime_dispatch_ipython_pause, a:text, "--", "\n"]
+                    else
+                    return a:text
+                    end
+                endfunction
             ]])
 
             local function mark_terminal()
