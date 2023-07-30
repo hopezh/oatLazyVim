258cf871c9be3a63c9942f866f7d9b6e00c8f55a
hopezh <hope.zh@gmail.com>
2023-07-30 18:41:33 +0800

    disable a few plugins

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,3 +1,5 @@
 return {
     { "nvim-neo-tree/neo-tree.nvim", enabled = false },
+    { "wthollingsworth/pomodoro.nvim", enabled = false },
+    { "AntonVanAssche/md-headers.nvim", enabled = false },
 }
