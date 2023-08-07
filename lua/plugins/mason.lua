5f53282120486a66ea3cd56cc51b66f7b83007b5
hopezh <hope.zh@gmail.com>
2023-08-07 23:46:50 +0800

    add r lsp server to mason

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -11,6 +11,7 @@ return {
             "marksman",
             "mypy",
             "pyright",
+            "r-languageserver",
             "ruff",
             "shellcheck",
             "shfmt",
