68b4098bacc062b0881114e166579b4775ec8f63
hopezh <hope.zh@gmail.com>
2023-08-14 23:18:50 +0800

    change tokyonight config

--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -17,9 +17,10 @@ return {
             -- Style to be applied to different syntax groups
             -- Value is any valid attr-list value for `:help nvim_set_hl`
             comments = { italic = true },
-            keywords = { italic = true, bold = true },
-            functions = { italic = true, bold = true },
-            variables = { italic = true },
+            keywords = { italic = true },
+            functions = { italic = true },
+            variables = { bold = true },
+
             -- Background styles. Can be "dark", "transparent" or "normal"
             sidebars = "transparent", -- style for sidebars, see below
             floats = "transparent", -- style for floajing windows
