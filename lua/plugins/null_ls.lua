e63924528bf949e5eb01d6803794b46b3750e71c
hopezh <hope.zh@gmail.com>
2023-09-04 20:58:48 +0800

    got error installing eslint in null-ls

--- lua/plugins/null_ls.lua
+++ lua/plugins/null_ls.lua
@@ -6,6 +6,7 @@ return {
     dependencies = { "mason.nvim" },
 
     opts = function()
+        local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
         local nls = require("null-ls")
 
         return {
@@ -20,7 +21,24 @@ return {
                 nls.builtins.formatting.stylua,
                 nls.builtins.formatting.shfmt,
                 -- nls.builtins.diagnostics.flake8,
+                -- nls.builtins.diagnostics.eslint, -- got error:"command eslint is not executable (make sure it's installed and on your $PATH)"
             },
+
+            -- on_attach = function(client, bufnr)
+            --     if client.supports_method("textDocument/formatting") then
+            --         vim.api.nvim_clear_autocmds({
+            --             group = augroup,
+            --             buffer = bufnr,
+            --         })
+            --         vim.api.nvim_create_autocmd("BufWritePre", {
+            --             group = augroup,
+            --             buffer = bufnr,
+            --             callback = function()
+            --                 vim.lsp.buf.format({ bufnr = bufnr })
+            --             end,
+            --         })
+            --     end
+            -- end,
         }
     end,
 }
