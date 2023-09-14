6a425a291245347b069b38983a40891592c5a37e
hopezh <hope.zh@gmail.com>
2023-09-14 14:41:47 +0800

    use color to differentiate indent areas

--- lua/plugins/indent_blankline.lua
+++ lua/plugins/indent_blankline.lua
@@ -2,26 +2,40 @@ return {
     { -- indent-blankline -------------------------------------------------------
         "lukas-reineke/indent-blankline.nvim",
 
-        -- method 1: use "opts{}" to change some options
-        opts = {
-            -- char = "│",
-            char = "▏", -- oat: indent symbol
-
-            show_current_context = true, -- oat: highlight the context
-            show_current_context_start = true,
-        },
+        -- -- method 1: use "opts{}" to change some options
+        -- opts = {
+        --     -- char = "│",
+        --     char = "▏", -- oat: indent symbol
+        --
+        --     show_current_context = true, -- oat: highlight the context
+        --     show_current_context_start = true,
+        -- },
 
         -- method 2: use "require("pluinName").setup({})" to change some options
-        -- config = function()
-        --     require("indent_blankline").setup({
-        --         char = "▏", -- indent symbol
-        --         -- char = "│",
-        --
-        --         show_current_context = true, -- highlight the context
-        --         show_current_context_start = false,
-        --
-        --         show_trailing_blankline_indent = false,
-        --     })
-        -- end,
+        config = function()
+            vim.opt.termguicolors = true
+            vim.cmd([[highlight IndentBlanklineIndent1 guibg=#2f2f2f gui=nocombine]])
+            -- vim.cmd([[highlight IndentBlanklineIndent2 guibg=#303030 gui=nocombine]])
+
+            require("indent_blankline").setup({
+                -- char = "▏", -- indent symbol
+                -- char = "│",
+                char = " ",
+
+                -- set custom bg colors for indent char and indent space
+                char_highlight_list = {
+                    "IndentBlanklineIndent1",
+                    "IndentBlanklineIndent2",
+                },
+                space_char_highlight_list = {
+                    "IndentBlanklineIndent1",
+                    "IndentBlanklineIndent2",
+                },
+
+                show_current_context = true, -- highlight the context
+                show_current_context_start = false,
+                show_trailing_blankline_indent = false,
+            })
+        end,
     },
 }
