8d28aa4ae7a9589add32a16b17738dc51fd9ab9e
hopezh <hope.zh@gmail.com>
2023-09-19 17:19:51 +0800

    use todo_comments successfully

--- test/test.py
+++ test/test.py
@@ -1,14 +1,16 @@
+# TODO: aaa
 class Person:
     def __init__(self, name, age):
         self.name = name
         self.age = age
 
+    # FIX: eee
     def myfunc(self):
         print("Hello my name is " + self.name)
 
 
+# NOTE: bbb
 p1 = Person("John", 36)
 p1.myfunc()
 
-# TODO aaa
-# NOTE bbb
+# WARN: ddd
