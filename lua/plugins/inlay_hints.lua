9951b2febdc102366f6cff1b8878de7d91c604b2
hopezh <hope.zh@gmail.com>
2023-09-20 22:15:04 +0800

    wait for nvim 0.10 to support inlay hints

---
+++ lua/plugins/inlay_hints.lua
@@ -0,0 +1,18 @@
+return {
+    -- need nvim >= 0.10 to support inlay_hints
+    -- {
+    --     "neovim/nvim-lspconfig",
+    --     opts = {
+    --         inlay_hints = { enabled = true },
+    --         servers = {
+    --             lua_ls = {
+    --                 settings = {
+    --                     lua = {
+    --                         hint = { enabled = true },
+    --                     },
+    --                 },
+    --             },
+    --         },
+    --     },
+    -- },
+}
