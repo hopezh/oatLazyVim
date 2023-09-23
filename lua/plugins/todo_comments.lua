b0249f17074ce36ed93fce4c069cb3231ae7a027
hopezh <hope.zh@gmail.com>
2023-09-23 23:07:55 +0800

    add custom todo keyword: oat

---
+++ lua/plugins/todo_comments.lua
@@ -0,0 +1,11 @@
+return {
+    "folke/todo-comments.nvim",
+
+    -- oat: add an "oat" comment keyword
+    opts = {
+        merge_keywords = true,
+        keywords = {
+            oat = { icon = "", color = "#ffa500" },
+        },
+    },
+}
