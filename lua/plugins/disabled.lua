c90b6510daaecb1f4049dd4bf43c15e0b25e970f
hopezh <hope.zh@gmail.com>
2023-09-25 23:43:24 +0800

    move lsp_signature to disable.lua

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -5,5 +5,6 @@ return {
     { "ggandor/flit.nvim", enabled = false },
     { "stevearc/dressing.nvim", enabled = false },
     { "sekke276/dark_flat.nvim", enabled = false },
+    { "ray-x/lsp_signature.nvim", enabled = false },
     -- { "olimorris/onedarkpro.nvim", enabled = false },
 }
