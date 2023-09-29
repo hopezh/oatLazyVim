f25454e989cb4979601c55b9f555a01bd7489d41
hopezh <hope.zh@gmail.com>
2023-09-29 22:33:21 +0800

    replace null-ls.nvim with none-ls.nvim

--- lua/plugins/null_ls.lua
+++ lua/plugins/null_ls.lua
@@ -1,5 +1,6 @@
 return {
-    "jose-elias-alvarez/null-ls.nvim",
+    -- "jose-elias-alvarez/null-ls.nvim",
+    "nvimtools/none-ls.nvim", -- after null-ls.nvim was shelfted
 
     event = { "BufReadPre", "BufNewFile" },
 
