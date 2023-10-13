31b8b373ac26287a7655372f669d0d28fa181ea4
hopezh <hope.zh@gmail.com>
2023-10-13 21:57:37 +0800

    enable alpha & none-ls in lazy.lua

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -19,8 +19,10 @@ require("lazy").setup({
         -- add LazyVim and import its plugins, only applicable when lazyvim is installed
         { "LazyVim/LazyVim", import = "lazyvim.plugins" },
         -- import any extras modules here
+        { import = "lazyvim.plugins.extras.lsp.none-ls" },
         { import = "lazyvim.plugins.extras.lang.typescript" },
         { import = "lazyvim.plugins.extras.lang.json" },
+        { import = "lazyvim.plugins.extras.ui.alpha" },
         -- { import = "lazyvim.plugins.extras.formatting.prettier" },
         -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
         -- import/override with your plugins
