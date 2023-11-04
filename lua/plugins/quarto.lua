d57d9bf4f8f107c65c956cb21743eb2fc1c238f3
hopezh <hope.zh@gmail.com>
2023-11-04 21:44:43 +0800

    move @param out of opts, and lsp is working for quarto, again!!!

--- lua/plugins/quarto.lua
+++ lua/plugins/quarto.lua
@@ -95,9 +95,11 @@ return {
 
     {
         "hrsh7th/nvim-cmp",
+
         dependencies = { "jmbuhr/otter.nvim" },
+
+        ---@param opts cmp.ConfigSchema
         opts = function(_, opts)
-            ---@param opts cmp.ConfigSchema
             local cmp = require("cmp")
             opts.sources = cmp.config.sources(vim.list_extend(opts.sources, { { name = "otter" } }))
 
